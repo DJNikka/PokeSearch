a052a16585820f09970b5ba895762d46e988c176
Nikka-Sher <npiterman@gmail.com>
2017-06-16 22:17:07 -0700

    Delegate setup

---
+++ PokeSearch/PokeSearch-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  PokeSearch.h
+//  PokeSearch
+//
+//  Created by Konstantine Piterman on 6/16/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+#ifndef PokeSearch_h
+#define PokeSearch_h
+
+
+#endif /* PokeSearch_h */
+
+#import "GeoFire.h"
